390368612f3bbd59aeb6b6ed064b18bc2c8df31c
d1makov <makovskyj.danylo@gmail.com>
2019-12-03 14:53:39 +0200

    final

--- cart.py
+++ cart.py
@@ -1,6 +1,49 @@
+"""
+docstring
+"""
+
+
 class Product:
-    pass
+    """
+    Product docstring
+    """
+
+    def __init__(self, name, price, qty):
+        """
+
+        :param name:
+        :param price:
+        :param qty:
+        """
+        self.name = name
+        self.price = price
+        self.qty = qty
 
 
 class Cart:
-    pass
+    """
+    cart docstring
+    """
+    def __init__(self, csv_name):
+        """
+
+        :param csv_name:
+        """
+        self.csv_name = csv_name
+        with open(self.csv_name, 'rt') as file:
+            self.csv = [line.split(',') for line in file.readlines()]
+
+    def calc_total(self):
+        """
+
+        :return:
+        """
+        return sum(float(line[1]) * int(line[2]) for line in self.csv)
+
+    def get_product(self, param):
+        """
+
+        :param param:
+        :return:
+        """
+        return Product(self.csv[param][0], float(self.csv[param][1]), int(self.csv[param][2]))
